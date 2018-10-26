622a36355be9a65914101568eb0fbd6715f27f4c
James Gallagher <jgallagher@opendap.org>
2018-10-25 18:52:12 -0600

    Added event stuff

--- src/com/tutorialspoint/MainApp.java
+++ src/com/tutorialspoint/MainApp.java
@@ -1,12 +1,17 @@
 package com.tutorialspoint;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
    public static void main(String[] args) {
-      ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+      ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+      
+      context.start();	// raise the start event
+      
       HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
       obj.getMessage();
+      
+      context.stop(); 	// raise the stop event
    }
 }
